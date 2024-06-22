b677fe40fe05faa141c6729be68668d073d629b3
Elena Komushyna <elena.komushyna@nexato.de>
2024-06-22 22:29:57 +0200

    added ios sync for multiple targets

--- src/sync-ios.ts
+++ src/sync-ios.ts
@@ -1,5 +1,5 @@
-import { resolve } from "path";
-import { readFileSync, writeFileSync } from "fs";
+import { resolve, join } from "path";
+import { readFileSync, writeFileSync, readdirSync, statSync } from "fs";
 
 import chalk from "chalk";
 import plist from "plist";
@@ -12,7 +12,26 @@ type PlistVersion = {
   CFBundleVersion: string;
 };
 
-const infoPlistPath = resolve(process.cwd(), "ios/App/App/Info.plist");
+function findFiles(matchingPattern: string): string[] {
+  let matchingFiles: string[] = [];
+
+  try {
+      const searchPath = resolve(process.cwd(), "ios/App/App")
+      const files = readdirSync(searchPath);
+      for (const file of files) {
+          let filePath = join(searchPath, file);
+          if (statSync(filePath).isFile() && file.includes(matchingPattern)) {
+              matchingFiles.push(filePath);
+          }
+      }
+  } catch (error) {
+      console.error(`Error while enumerating files in  "ios/App/App": ${error}`);
+  }
+
+  return matchingFiles;
+}
+
+const infoPlistPaths = findFiles('Info.plist');
 
 export const syncIos = async () => {
   const { version } = readPackage(resolve(process.cwd(), "package.json"));
@@ -22,15 +41,20 @@ export const syncIos = async () => {
     process.exit();
   }
 
-  let content = readFileSync(infoPlistPath, { encoding: "utf8" });
+  infoPlistPaths.forEach(infoPlistPath => {
+    try {
+        let content = readFileSync(infoPlistPath, { encoding: 'utf8' });
 
-  const infoPlist = plist.parse(content) as PlistVersion;
+        const infoPlist = plist.parse(content) as PlistVersion;  // Adjust the type as necessary
 
-  infoPlist.CFBundleShortVersionString = version;
-  infoPlist.CFBundleVersion = version;
+        infoPlist.CFBundleShortVersionString = version;
 
-  content = plist.build(infoPlist);
+        content = plist.build(infoPlist);
 
-  writeFileSync(infoPlistPath, `${content}\n`, { encoding: "utf8" });
-  log(chalk`{green ✔} Sync version ${version} for ios.`);
+        writeFileSync(infoPlistPath, `${content}\n`, { encoding: 'utf8' });
+        log(chalk`{green ✔} Sync version ${version} for ios in ${infoPlistPath}.`);
+    } catch (error) {
+        console.error(`Error processing ${infoPlistPath}: ${error}`);
+    }
+});
 };
